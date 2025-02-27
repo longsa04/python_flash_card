72aadca9d6bcb4ddcfb5bd62658cb03550400b6c
longsa04 <teatisa12345@gmail.com>
2025-02-27 14:19:53 +0700

    Initial commit

--- main.py
+++ main.py
@@ -16,8 +16,6 @@ else:
     to_learn = data.to_dict(orient="records")
 
 
-
-
 def next_card():
     global current_card, flip_timer
     window.after_cancel(flip_timer)
@@ -27,10 +25,12 @@ def next_card():
     canvas.itemconfig(card_background, image=card_front_img)
     flip_timer = window.after(3000, func=flip_card)
 
+
 def flip_card():
     canvas.itemconfig(card_title, text="English", fill="white")
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_background, image=card_back_img)
+
 
 def is_known():
     to_learn.remove(current_card)
@@ -42,8 +42,6 @@ def is_known():
 
 window = Tk()
 window.title("Flashy")
-
-
 
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
@@ -59,7 +57,7 @@ canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
 cross_image = PhotoImage(file="images/wrong.png")
-unknown_button = Button(image=cross_image, highlightthickness=0, command= next_card)
+unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
 unknown_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file="images/right.png")
@@ -67,7 +65,5 @@ known_button = Button(image=check_image, highlightthickness=0, command=is_known)
 known_button.grid(row=1, column=1)
 
 next_card()
-
-
 
 window.mainloop()
